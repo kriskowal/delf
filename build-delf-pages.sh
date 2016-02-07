7a6a9f2c69b10108ec45c1891b29ee37db363398
Kris Kowal <kris@uber.com>
2016-02-06 16:05:02 -0800

    Add scripts necessary for bundling

--- build-delf-pages.sh
+++ build-delf-pages.sh
@@ -6,16 +6,34 @@ export PATH="$HERE/node_modules/.bin":"$PATH"
 export GIT_DIR="$HERE/.git"
 export GIT_INDEX_FILE=$(mktemp "$GIT_DIR/TEMP.XXXXXX")
 
-function gentree() {
-    echo "100644 blob $(git hash-object -w <(bundle essays/digger/index.js))"$'\t'"bundle.js"
-    echo "100644 blob $(git hash-object -w fusion.js)"$'\t'"fusion.js"
-    echo "100644 blob $(git hash-object -w essays/digger/index.css)"$'\t'"index.css"
-    echo "100644 blob $(git hash-object -w node_modules/colorim.html/index.css)"$'\t'"colorim.css"
-    echo "100644 blob $(git hash-object -w essays/digger/bundle.html)"$'\t'"index.html"
-    echo "100644 blob $(git hash-object -w essays/digger/CNAME)"$'\t'"CNAME"
+function blob() {
+    # usage: blob entry source
+    # generates an entry for a tree, suitable for piping into git mktree.
+    # e.g., blob bundle.js <(bundle essays/digger/index.js)
+    echo "100644 blob $(git hash-object -w $2)"$'\t'"$1"
 }
 
-OVERLAY=$(gentree | git mktree)
+function tree() {
+    # usage: tree entry source
+    # generates an entry for a subtree, suitable for piping into mktree.
+    # the source must also be a file/named-pipe suitable for piping into
+    # mktree.
+    # e.g., tree docs <(gendocs)
+    echo "040000 tree $(git mktree < $2)"$'\t'"$1"
+}
+
+function genroot() {
+    # generates the root directory for delve build products.
+    blob CNAME essays/digger/CNAME
+    blob fusion.js fusion.js
+    blob fusion.js.map fusion.js.map
+    blob index.html essays/digger/bundle.html
+    blob index.css essays/digger/index.css
+    blob colorim.css node_modules/colorim.html/index.css
+    blob bundle.js <(bundle essays/digger/index.js)
+}
+
+OVERLAY=$(genroot | git mktree)
 git read-tree --empty
 git read-tree --prefix=/ $OVERLAY
 TREE=$(git write-tree --missing-ok)
